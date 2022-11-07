396e5da9775e723198902555ad6574c81e8b4514
jhologic12 <jhonospino@gmail.com>
2022-11-07 17:24:12 -0500

    agregan dia semana y dia mes

---
+++ js/reloj.js
@@ -0,0 +1,33 @@
+
+const mostrarReloj = ()=>{
+
+
+    let fecha = new Date ();
+
+    let hora = formatoHora(fecha.getHours());
+    let min = formatoHora(fecha.getMinutes());
+    let seg = formatoHora(fecha.getSeconds());
+
+    document.getElementById('hora').innerHTML = `${hora}:${min}:${seg}`;
+
+    const meses = ['Ene','Feb','Marz','Abr','May','Jun','Jul','Ago','Sep','Oct','Nov','Dic'];
+    const dias = ['Dom', 'Lun', 'Mar','Mie','Jue','Vie','Sab'];
+    let diaSemana = fecha.getDate();
+    let mes = meses[fecha.getMonth()];
+    let dia = dias[fecha.getDay()];
+
+    let fechaTexto = `${dia},${diaSemana}  ${mes}`;
+
+    document.getElementById('fecha').innerHTML=fechaTexto;
+
+
+
+}
+
+const formatoHora = (hora)=>{
+    if(hora<10)
+        hora = '0' + hora;
+        return hora;
+}
+
+setInterval(mostrarReloj,1000);
